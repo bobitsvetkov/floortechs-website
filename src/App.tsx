c20cae885da7d931fef37a4ced55d74499b4415f
Borislav <borislav.tsvetkov02@gmail.com>
2025-01-09 22:00:06 +0200

    test: test env var change

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Contact from './components/Contact/Contact';
 import { LoadScript, Libraries } from '@react-google-maps/api';
 import Footer from './components/Footer/Footer';
 
-const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string || process.env.GOOGLE_MAPS_API_KEY as string;
+const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string
 const GOOGLE_MAPS_LIBRARIES: Libraries = ['places', 'drawing', 'geometry', 'visualization'];
 
 const App = () => {
