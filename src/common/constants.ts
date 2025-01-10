b19ad12ca329c189dbc40fd6e7cdb9926bec3056
Borislav <borislav.tsvetkov02@gmail.com>
2025-01-10 20:32:22 +0200

    add: add constants file

---
+++ src/common/constants.ts
@@ -0,0 +1,4 @@
+import { Libraries } from "@react-google-maps/api";
+const GOOGLE_MAPS_LIBRARIES: Libraries = ['places', 'drawing', 'geometry', 'visualization'];
+
+export default GOOGLE_MAPS_LIBRARIES
