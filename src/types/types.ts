820ed15c46a3a13cd7c84d6a2c818c7e0a974e1a
Borislav Tsvetkov <borislav.tsvetkov02@gmail.com>
2025-03-03 11:06:12 +0200

    add: add flooring solution types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,9 +1,23 @@
-export type Page = 'home' | 'solutions' | 'work' | 'installation' | 'contact';
+export type Page = "home" | "solutions" | "work" | "installation" | "contact";
 
 export interface Credentials {
-    emailJsPublicKey: string;
-    emailJsServiceKey: string;
-    emailJsTemplateKey: string;
-    recaptchaSiteKey: string;
-    googleMapsApiKey: string;
+  emailJsPublicKey: string;
+  emailJsServiceKey: string;
+  emailJsTemplateKey: string;
+  recaptchaSiteKey: string;
+  googleMapsApiKey: string;
+}
+
+interface Industry {
+  name: string;
+  description: string;
+}
+export interface FlooringSolution {
+  id: string;
+  title: string;
+  tagline: string;
+  description: string;
+  idealFor: Industry[];
+  imageUrl: string;
+  primaryColor: string;
 }
