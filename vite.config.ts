78a1b169e5ab956fe1d494b424e5ba67a8c00d5c
Borislav Tsvetkov <borislav.tsvetkov02@gmail.com>
2025-03-03 20:46:49 +0200

    add: add manual chunks config for vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: "./",
+  build: {
+    target: "esnext",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        }
+      }
+    }
+  }
 })
