74d9f62364ceffce0999fb8f0d77ce6fd08a3fc6
Borislav Tsvetkov <borislav.tsvetkov02@gmail.com>
2025-06-20 21:46:16 +0300

    deprecate custom hook

--- src/hooks/useCredentials.ts
+++ src/hooks/useCredentials.ts
@@ -1,34 +1,37 @@
-import { useState, useEffect } from 'react';
-import type { Credentials } from '../types/types';
+// DEPRECATED: This hook is no longer in use due to exposing sensitive info to frontend. Might be used in future when I manage to host backend server to handle sensitive data securely.
 
-export const useCredentials = () => {
-    const [credentials, setCredentials] = useState<Credentials | null>(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        try {
-            const localCredentials: Credentials = {
-                emailJsPublicKey: import.meta.env.VITE_EMAIL_JS_PUBLIC_KEY,
-                emailJsServiceKey: import.meta.env.VITE_EMAIL_JS_SERVICE_KEY,
-                emailJsTemplateKey: import.meta.env.VITE_EMAIL_JS_TEMPLATE_KEY,
-                recaptchaSiteKey: import.meta.env.VITE_RECAPTCHA_SITE_KEY,
-                googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-            };
+// import { useState, useEffect } from 'react';
+// import type { Credentials } from '../types/types';
 
-            const hasMissing = Object.values(localCredentials).some(v => !v);
-            if (hasMissing) {
-                throw new Error('Missing one or more environment variables');
-            }
-            setCredentials(localCredentials);
-            setError(null);
-        } catch (e) {
-            console.error('Error loading credentials from env:', e);
-            setError('Missing or invalid environment variables');
-        } finally {
-            setIsLoading(false);
-        }
-    }, []);
+// export const useCredentials = () => {
+//     const [credentials, setCredentials] = useState<Credentials | null>(null);
+//     const [isLoading, setIsLoading] = useState(true);
+//     const [error, setError] = useState<string | null>(null);
 
-    return { credentials, isLoading, error };
-};
+//     useEffect(() => {
+//         try {
+//             const localCredentials: Credentials = {
+//                 emailJsPublicKey: import.meta.env.VITE_EMAIL_JS_PUBLIC_KEY,
+//                 emailJsServiceKey: import.meta.env.VITE_EMAIL_JS_SERVICE_KEY,
+//                 emailJsTemplateKey: import.meta.env.VITE_EMAIL_JS_TEMPLATE_KEY,
+//                 recaptchaSiteKey: import.meta.env.VITE_RECAPTCHA_SITE_KEY,
+//                 googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+//             };
+
+//             const hasMissing = Object.values(localCredentials).some(v => !v);
+//             if (hasMissing) {
+//                 throw new Error('Missing one or more environment variables');
+//             }
+//             setCredentials(localCredentials);
+//             setError(null);
+//         } catch (e) {
+//             console.error('Error loading credentials from env:', e);
+//             setError('Missing or invalid environment variables');
+//         } finally {
+//             setIsLoading(false);
+//         }
+//     }, []);
+
+//     return { credentials, isLoading, error };
+// };
